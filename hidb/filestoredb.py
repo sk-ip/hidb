28e5c27e11199b8ce193a47342863eec4b96ed81
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-12-29 17:41:09 +0530

    hidb: filestoredb: Raise errors instead of return

--- hidb/filestoredb.py
+++ hidb/filestoredb.py
@@ -18,36 +18,32 @@ class fileStoreDB(DB):
     def showData(self, key: str):
         currtime = datetime.now().time()
         datatime = self.filestore.data[key].timestamp
-
-        if currtime - datatime > self.ttl:
-            self.delete(key)
-            return KeyError(f"{key} does not exist")
         return self.filestore.data[key].data
 
     def saveData(self):
         pass
 
-    def create(self, key: str, value: Dict, ttl: int):
+    def create(self, key: str, value: Dict, ttl: int = None):
         if len(key) > 32:
-            return KeyError(f"Length of key: {len(key)} exceeds limits should be: 32")
+            raise KeyError(f"Length of key: {len(key)} exceeds limits should be: 32")
         if key in self.filestore.keys:
-            return KeyError(f"{key} does not exist")
+            raise KeyError(f"{key} does not exist")
         self.addNewData(key, value, ttl)
         self.saveData()
 
     def read(self, key: str):
         if len(key) > 32:
-            return KeyError(f"Length of key: {len(key)} exceeds limits should be: 32")
+            raise KeyError(f"Length of key: {len(key)} exceeds limits should be: 32")
         if key not in self.filestore.keys:
-            return KeyError(f"{key} does not exist")
+            raise KeyError(f"{key} does not exist")
 
-        self.showData(key)
+        return self.showData(key)
 
     def delete(self, key: str):
         if len(key) > 32:
-            return KeyError(f"Length of key: {len(key)} exceeds limits should be: 32")
+            raise KeyError(f"Length of key: {len(key)} exceeds limits should be: 32")
         if key not in self.filestore.keys:
-            return KeyError(f"{key} does not exist")
+            raise KeyError(f"{key} does not exist")
         self.filestore.keys.remove(key)
         del self.filestore.data[key]
         self.saveData()
