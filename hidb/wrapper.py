3af6f401d439d66405618d7ab47f4cd67e0f7c25
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-12-31 14:12:26 +0530

    hidb: wrapper: Add the memory constraints

--- hidb/wrapper.py
+++ hidb/wrapper.py
@@ -5,6 +5,11 @@ class fileWrapper(object):
     def __init__(self):
         self.data = {}
         self.keys = set()
+        # JSON data size 16KB in Bytes
+        self.max_data_size = 16384
+        # Max database size 1GB in Bytes
+        self.max_database_size = 1073741824
+        self.current_database_size = 0
 
 
 class dataWrapper:
